00526710f68874c34b285eb05487097741225d0b
netil <jaesung.park@navercorp.com>
2015-04-20 15:55:20 +0900

    separated as external script file for loading module and starting unit test

---
+++ test/asset/load_run.js
@@ -0,0 +1,9 @@
+﻿(function() {
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = "tc/"+ _sModule +".js";
+
+    document.head.appendChild(s);
+})();
+
+QUnit.start();
