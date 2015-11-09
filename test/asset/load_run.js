893f362ff49a6cf02a5ae653a3abc2f0119c3637
Jae Sung Park <alberto.park@gmail.com>
2015-11-09 18:36:05 +0900

    changed the way of start test call function

--- test/asset/load_run.js
+++ test/asset/load_run.js
@@ -1,9 +1,15 @@
-﻿(function() {
+﻿(function(callback) {
     var s = document.createElement("script");
     s.type = "text/javascript";
     s.src = "tc/"+ _sModule +".js";
 
-    document.head.appendChild(s);
-})();
+    (document.head || document.getElementsByTagName("head")[0]).appendChild(s);
 
-QUnit.start();
+    if(s.addEventListener) {
+        s.onload = callback;
+    } else if(s.readyState) {
+        s.onreadystatechange = function() {
+            s.readyState === "loaded" && callback();
+        };
+    }
+})(QUnit.start);
